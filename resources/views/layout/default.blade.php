ce1c9c26f52f087c3e411b07258505739922d24d
Kinzuka <putraantowi11@gmail.com>
2023-07-12 07:59:56 +0700

    t

---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=1920, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sirapi</title>
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.16.22/dist/css/uikit.min.css" />
+    
+    <!-- UIkit JS -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.22/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.22/dist/js/uikit-icons.min.js"></script>
+</head>
+<body>
+    @include('layout.header')
+</body>
+</html>
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,48 @@
+<nav class="uk-navbar-container">
+    <div class="uk-container">
+        <div uk-navbar>
+
+            <div class="uk-navbar-left">
+
+                <a class="uk-navbar-item uk-logo" href="#" aria-label="Back to Home">
+                    <img src="{{url('/images/logo.png')}}" alt="logo">
+                </a>
+
+                <ul class="uk-navbar-nav">
+                    <li>
+                        <a href="#">
+                            Beranda
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Cari Proyek
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Cari Supplier
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
+
+            <div class="uk-navbar-right">
+
+                <div class="uk-navbar-item">
+                    <div>Some <a href="#">Link</a></div>
+                </div>
+
+                <div class="uk-navbar-item">
+                    <form action="javascript:void(0)">
+                        <input class="uk-input uk-form-width-small" type="text" placeholder="Input" aria-label="Input">
+                        <button class="uk-button uk-button-default">Button</button>
+                    </form>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</nav>
